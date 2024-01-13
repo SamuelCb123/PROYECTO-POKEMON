e21cbfec42480b0be7ecab31ea69e636225fd210
Samuel Castro <dreift133@gmail.com>
2024-01-13 23:53:00 +0100

    funcion para el footer creada

--- script1.js
+++ script1.js
@@ -1,28 +1,46 @@
 "use strict";
-check.onclick = togglePassword;
 
-function togglePassword() {
-    if (check.checked) {
-        pass.type = "text";
-    } else {
-        pass.type = "password";
+document.addEventListener('DOMContentLoaded', function() {
+    
+    let check = document.getElementById("check");
+    let pass = document.getElementById("pass");
+    let registroButton = document.getElementById('registro');
+    let iniciarsesion = document.getElementById('iniciarsesion');
+    let año = document.getElementById("año");
+
+    if (check && pass) {
+        check.onclick = togglePassword;
     }
-}
-let registroButton = document.getElementById('registro');
-let iniciarsesion = document.getElementById('iniciarsesion');
 
-if (registroButton) {
-    registroButton.addEventListener('click', function() {
-        window.location.href = 'iniciarsesion.html';
-    });
-} else {
-    console.error('No hay elemento registro.');
-}
+    function togglePassword() {
+        if (check.checked) {
+            pass.type = "text";
+        } else {
+            pass.type = "password";
+        }
+    }
 
-if (iniciarsesion) {
-    iniciarsesion.addEventListener('click', function() {
-        window.location.href = 'perfil.html';
-    });
-} else {
-    console.error('No hay elemento iniciarsesion.');
-}
+    if (registroButton) {
+        registroButton.addEventListener('click', function() {
+            window.location.href = 'iniciarsesion.html';
+        });
+    } else {
+        console.error('No hay elemento registro.');
+    }
+
+    if (iniciarsesion) {
+        iniciarsesion.addEventListener('click', function() {
+            window.location.href = 'perfil.html';
+        });
+    } else {
+        console.error('No hay elemento iniciarsesion.');
+    }
+
+    let añoActual = new Date().getFullYear();
+
+    if (año) {
+        año.textContent = añoActual;
+    } else {
+        console.error('No hay elemento año.');
+    }
+});

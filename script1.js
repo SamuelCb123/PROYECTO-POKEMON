5f9f458f7fcf5e87d14d85645930d339416410f5
Samuel Castro <dreift133@gmail.com>
2024-01-13 19:48:56 +0100

    Registro e inicio sesion acabados a nivel de diseño falta validarlos

--- script1.js
+++ script1.js
@@ -8,3 +8,21 @@ function togglePassword() {
         pass.type = "password";
     }
 }
+let registroButton = document.getElementById('registro');
+let iniciarsesion = document.getElementById('iniciarsesion');
+
+if (registroButton) {
+    registroButton.addEventListener('click', function() {
+        window.location.href = 'iniciarsesion.html';
+    });
+} else {
+    console.error('No hay elemento registro.');
+}
+
+if (iniciarsesion) {
+    iniciarsesion.addEventListener('click', function() {
+        window.location.href = 'perfil.html';
+    });
+} else {
+    console.error('No hay elemento iniciarsesion.');
+}

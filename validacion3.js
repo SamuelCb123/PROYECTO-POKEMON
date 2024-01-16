d87464ca01bdd85202f0d3b06c63b091b5295085
Samuel Castro <dreift133@gmail.com>
2024-01-16 18:08:32 +0100

    formulario para crear pkm y validado

---
+++ validacion3.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const form = document.getElementById('pokemon'); 
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let name = document.getElementById('name');
+    let type = document.getElementById('type');
+    let type2 = document.getElementById('type2');
+    let region = document.getElementById('region');
+    let warnings = document.getElementById('warnings');
+    let warningsText = "";
+
+    if (name.value.trim() === "") {
+        warningsText += `El campo "Nombre" no puede estar vacío <br>`;
+    } else if (name.value.length < 3) {
+        warningsText += `El nombre es demasiado corto <br>`;
+    }
+
+    if (type.value.trim() === "") {
+        warningsText += `El campo "Tipo" no puede estar vacío <br>`;
+    } else if (type.value.length < 3) {
+        warningsText += `El tipo es demasiado corto <br>`;
+    }
+
+    if (type2.value.trim() === "") {
+        warningsText += `El campo "Tipo 2" no puede estar vacío <br>`;
+    } else if (type2.value.length < 3) {
+        warningsText += `El tipo 2 es demasiado corto <br>`;
+    }
+
+    if (region.value.trim() === "") {
+        warningsText += `El campo "Región" no puede estar vacío <br>`;
+    } else if (region.value.length < 3) {
+        warningsText += `La región es demasiado corta <br>`;
+    }
+
+    if (warningsText === "") {
+        // All validation passed, show alert
+        alert("Pokemon creado");
+    } else {
+        // Display validation warnings
+        warnings.innerHTML = warningsText;
+    }
+});

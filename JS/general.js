2568100fdc86772a7f78f3bd9719f6eba8ca504e
Samuel Castro <dreift133@gmail.com>
2024-01-17 23:31:13 +0100

    comentarios

--- JS/general.js
+++ JS/general.js
@@ -3,6 +3,6 @@ function eliminarFila(button) {//creacion de una funcion a la que llamaremos eli
     let confirmacion = confirm("¿Estás seguro de que quieres eliminar esta fila?");//creamos una variable llamada confirmacion a la que asignamos
     if (confirmacion) {              //que muestra un cuadro dialogo.El resultado se guardara en la variable confirmacion
        let row = button.parentNode.parentNode;//si se acepta la fila se eliminara para esto obtenemos el nodo padre del boton
-        row.parentNode.removeChild(row);//y utilizaremos remove para eliminar la fila
+        row.parentNode.removeChild(row);//y utilizaremos remove para eliminar la fila sd
     }
 }
